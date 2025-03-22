297a9bd3023fee996b6908071b8dd9e82bd96946
PHANUWAT\panuw <Panuwat_nine@hotmail.com>
2025-03-22 19:56:22 +0700

    update notification

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_timezone/flutter_timezone.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 import 'package:flutter/material.dart';
@@ -12,28 +13,67 @@ class NotificationService {
 
   NotificationService._internal();
 
-  final FlutterLocalNotificationsPlugin _notificationsPlugin = FlutterLocalNotificationsPlugin();
+  final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
+  // initialization
   Future<void> init() async {
+    // initialize timezone handling
     tz.initializeTimeZones();
+    final String currentTimeZone = await FlutterTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(currentTimeZone));
 
-    tz.setLocalLocation(tz.getLocation("Asia/Bangkok"));
+    // initialize android
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
 
-    const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
-    final DarwinInitializationSettings initializationSettingsIOS = DarwinInitializationSettings(requestSoundPermission: false, requestBadgePermission: false, requestAlertPermission: false);
-    final InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    // initialize ios
+    final DarwinInitializationSettings initializationSettingsIOS =
+        DarwinInitializationSettings(
+            requestSoundPermission: false,
+            requestBadgePermission: false,
+            requestAlertPermission: false);
 
+    // initialize settings
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: initializationSettingsAndroid,
+            iOS: initializationSettingsIOS);
+
+    // initialize
     await _notificationsPlugin.initialize(initializationSettings);
   }
 
-  Future<void> scheduleDailyNotification(DateTime selectedTime) async {
+  // immediate notification
+  Future<void> showNotification({
+    int id = 0,
+    required String title,
+    required String body,
+    String? payload,
+  }) {
+    return _notificationsPlugin.show(id, title, body, _notificationDetails());
+  }
+
+  // schedule notification
+  Future<void> scheduleDailyNotification(
+    DateTime selectedTime, {
+    int id = 1,
+    required String title,
+    required String body,
+    String? payload,
+  }) async {
     if (selectedTime.isBefore(DateTime.now())) {
       selectedTime = selectedTime.add(const Duration(days: 1));
     }
-    final tz.TZDateTime scheduledTime = tz.TZDateTime.from(selectedTime, tz.local);
+
+    final tz.TZDateTime scheduledTime =
+        tz.TZDateTime.from(selectedTime, tz.local);
 
     try {
-      await _notificationsPlugin.zonedSchedule(0, "notification title", "notification body", scheduledTime, _notificationDetails(), androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle, matchDateTimeComponents: DateTimeComponents.time);
+      await _notificationsPlugin.zonedSchedule(
+          id, title, body, scheduledTime, _notificationDetails(),
+          androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle,
+          matchDateTimeComponents: DateTimeComponents.time);
 
       debugPrint('Notification scheduled successfully');
     } catch (e) {
@@ -41,7 +81,24 @@ class NotificationService {
     }
   }
 
+  // cancel notification
+  Future<void> cancelNotification(int id, {String? tag}) async {
+    await _notificationsPlugin.cancel(id, tag: tag);
+  }
+
+  // cancel all notification
+  Future<void> cancelAllNotification() async {
+    await _notificationsPlugin.cancelAll();
+  }
+
+  // notification detail
   NotificationDetails _notificationDetails() {
-    return NotificationDetails(android: AndroidNotificationDetails('your_channel_id', 'your_channel_name', importance: Importance.max, priority: Priority.high, showWhen: false), iOS: DarwinNotificationDetails());
+    return NotificationDetails(
+        android: AndroidNotificationDetails(
+            'your_channel_id', 'your_channel_name',
+            importance: Importance.max,
+            priority: Priority.high,
+            showWhen: true),
+        iOS: DarwinNotificationDetails());
   }
 }
